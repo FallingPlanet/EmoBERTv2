9d1cc5fc47124c8f1331ead1937066724022e383
WilliamAStigall <126931948+WilliamAStigall@users.noreply.github.com>
2023-12-06 14:34:12 -0500

    Fixed Problems (only datasets left)

--- EmoBERT_Tiny/tiny_trainer.py
+++ EmoBERT_Tiny/tiny_trainer.py
@@ -39,147 +39,147 @@ class Classifier:
     
     def train_step(self, dataloader, optimizer, epoch):
         self.model.train()
-        total_loss = 0
-        
-        pbar = tqdm(dataloader, desc = f"Training Epoch {epoch}")
-        
+        total_loss = 0.0
+        # Initialize metric accumulators
+        total_accuracy = 0.0
+        total_precision = 0.0
+        total_recall = 0.0
+        total_f1 = 0.0
+        total_mcc = 0.0
+
+        pbar = tqdm(dataloader, desc=f"Training Epoch {epoch}")
+
         for batch in pbar:
             input_ids, attention_masks, labels = [x.to(self.device) for x in batch]
-            
+
             optimizer.zero_grad()
-            
-            outputs = self.model(input_ids,attention_masks=attention_masks)
+            outputs = self.model(input_ids, attention_masks=attention_masks)
             loss = self.compute_loss(outputs.logits, labels)
             loss.backward()
-            
             optimizer.step()
-            total_loss = loss.item()
-           
-           
-            accuracy = self.accuracy(outputs.logits.argmax(dim=1), labels)
-            precision = self.precision(outputs.logits.argmax(dim=1), labels)
-            recall = self.recall(outputs.logits.argmax(dim=1), labels)
-            f1 = self.f1(outputs.logits, labels)
-            mcc = self.mcc(outputs.logits.argmax(dim=1), labels)
-            
+
+            total_loss += loss.item()
+
+            # Update and accumulate metrics
+            total_accuracy += self.accuracy(outputs.logits.argmax(dim=1), labels).item()
+            total_precision += self.precision(outputs.logits.argmax(dim=1), labels).item()
+            total_recall += self.recall(outputs.logits.argmax(dim=1), labels).item()
+            total_f1 += self.f1(outputs.logits, labels).item()
+            total_mcc += self.mcc(outputs.logits.argmax(dim=1), labels).item()
+
             # Update tqdm description with current loss and metrics
-            pbar.set_postfix({
-                'Loss': f'{total_loss / (pbar.n + 1):.4f}',
-                'Acc': f'{accuracy:.4f}',
-                'Prec': f'{precision:.4f}',
-                'Rec': f'{recall:.4f}',
-                'F1': f'{f1:.4f}',
-                'MCC': f'{mcc:.4f}'
-        })
-            pbar.update(1)
-             # Log metrics to TensorBoard
-            self.writer.log_scalar('Training/Loss', loss, epoch)
-            self.writer.log_scalar('Training/Accuracy', accuracy, epoch)
-            self.writer.log_scalar('Training/Precision', precision, epoch)
-            self.writer.log_scalar('Training/Recall', recall, epoch)
-            self.writer.log_scalar('Training/F1', f1, epoch)
-            self.writer.log_scalar('Training/MCC', mcc, epoch)
+            pbar.set_postfix(loss=total_loss / (pbar.n + 1))
+
+        # Calculate averages
+        num_batches = len(dataloader)
+        avg_accuracy = total_accuracy / num_batches
+        avg_precision = total_precision / num_batches
+        avg_recall = total_recall / num_batches
+        avg_f1 = total_f1 / num_batches
+        avg_mcc = total_mcc / num_batches
+        avg_train_loss = total_loss / num_batches
+
+        # Log metrics to TensorBoard
+        self.writer.log_scalar('Training/Average Loss', avg_train_loss, epoch)
+        self.writer.log_scalar('Training/Average Accuracy', avg_accuracy, epoch)
+        self.writer.log_scalar('Training/Average Precision', avg_precision, epoch)
+        self.writer.log_scalar('Training/Average Recall', avg_recall, epoch)
+        self.writer.log_scalar('Training/Average F1', avg_f1, epoch)
+        self.writer.log_scalar('Training/Average MCC', avg_mcc, epoch)
 
         pbar.close()
-        avg_train_loss = total_loss / len(dataloader)
-        self.writer.log_scalar('Training/Loss', avg_train_loss, epoch)  
-    def val_step(self, dataloader, epoch):
-        pbar = tqdm(dataloader,desc=f"Validation {epoch}")
-        self.model(eval)
-        total_loss = 0.0
-        
-        with torch.no_grad():
-                    for batch in pbar:
-                        input_ids, attention_masks, labels = [x.to(self.device) for x in batch]
-                        
-                     
-                        outputs = self.model(input_ids,attention_masks=attention_masks)
-                        loss = self.compute_loss(outputs.logits, labels)
-                        total_loss = loss.item()
-                    
-                    
-                        accuracy = self.accuracy(outputs.logits.argmax(dim=1), labels)
-                        precision = self.precision(outputs.logits.argmax(dim=1), labels)
-                        recall = self.recall(outputs.logits.argmax(dim=1), labels)
-                        f1 = self.f1(outputs.logits, labels)
-                        mcc = self.mcc(outputs.logits.argmax(dim=1), labels)
-                        
-                        # Update tqdm description with current loss and metrics
-                        pbar.set_postfix({
-                            'Loss': f'{total_loss / (pbar.n + 1):.4f}',
-                            'Acc': f'{accuracy:.4f}',
-                            'Prec': f'{precision:.4f}',
-                            'Rec': f'{recall:.4f}',
-                            'F1': f'{f1:.4f}',
-                            'MCC': f'{mcc:.4f}'
-                    })
-                        pbar.update(1)
-                        # Log metrics to TensorBoard
-                        self.writer.log_scalar('Validation/Loss', loss, epoch)
-                        self.writer.log_scalar('Validation/Accuracy', accuracy, epoch)
-                        self.writer.log_scalar('Validation/Precision', precision, epoch)
-                        self.writer.log_scalar('Validation/Recall', recall, epoch)
-                        self.writer.log_scalar('Validation/F1', f1, epoch)
-                        self.writer.log_scalar('Validation/MCC', mcc, epoch)
 
-                    pbar.close()
-                    avg_val_loss = total_loss / len(dataloader)
-                    self.writer.log_scalar('Validation/Loss', avg_val_loss, epoch)
-                    return avg_val_loss  
-    def test_step(self, dataloader):
+
+    def val_step(self, dataloader, epoch):
         self.model.eval()
-        aggregated_metrics = {}
-        
-        aggregated_metrics = {
-            'accuracy':  0.0,
-            'precision': 0.0,
-            'recall': 0.0,
-            'f1': 0.0,
-            'mcc': 0.0,
-            'top_2_acc': 0.0
-        }
-        
+        total_loss = 0.0
+        # Initialize metric accumulators
+        total_accuracy = 0.0
+        total_precision = 0.0
+        total_recall = 0.0
+        total_f1 = 0.0
+        total_mcc = 0.0
+
         with torch.no_grad():
-            pbar = tqdm(dataloader, desc="Testing")
-            
+            pbar = tqdm(dataloader, desc=f"Validation Epoch {epoch}")
             for batch in pbar:
                 input_ids, attention_masks, labels = [x.to(self.device) for x in batch]
                 
-                outputs = self.model(input_ids,attention_masks,labels)
-                accuracy = self.accuracy(outputs.logits.argmax(dim=1), labels)
-                precision = self.precision(outputs.logits.argmax(dim=1), labels)
-                recall = self.recall(outputs.logits.argmax(dim=1), labels)
-                f1 = self.f1(outputs.logits, labels)
-                mcc = self.mcc(outputs.logits.argmax(dim=1), labels)
-                top_2_acc = self.top2_acc(outputs.logits,labels).item()
-                
-                
+                outputs = self.model(input_ids, attention_masks=attention_masks)
+                loss = self.compute_loss(outputs.logits, labels)
+
+                total_loss += loss.item()
+
+                # Update and accumulate metrics
+                total_accuracy += self.accuracy(outputs.logits.argmax(dim=1), labels).item()
+                total_precision += self.precision(outputs.logits.argmax(dim=1), labels).item()
+                total_recall += self.recall(outputs.logits.argmax(dim=1), labels).item()
+                total_f1 += self.f1(outputs.logits, labels).item()
+                total_mcc += self.mcc(outputs.logits.argmax(dim=1), labels).item()
+
+                # Update tqdm description with current loss and metrics
+                pbar.set_postfix(loss=total_loss / (pbar.n + 1))
+
+        # Calculate averages
+        num_batches = len(dataloader)
+        avg_val_loss = total_loss / num_batches
+        avg_accuracy = total_accuracy / num_batches
+        avg_precision = total_precision / num_batches
+        avg_recall = total_recall / num_batches
+        avg_f1 = total_f1 / num_batches
+        avg_mcc = total_mcc / num_batches
+
+        # Log metrics to TensorBoard
+        self.writer.log_scalar('Validation/Average Loss', avg_val_loss, epoch)
+        self.writer.log_scalar('Validation/Average Accuracy', avg_accuracy, epoch)
+        self.writer.log_scalar('Validation/Average Precision', avg_precision, epoch)
+        self.writer.log_scalar('Validation/Average Recall', avg_recall, epoch)
+        self.writer.log_scalar('Validation/Average F1', avg_f1, epoch)
+        self.writer.log_scalar('Validation/Average MCC', avg_mcc, epoch)
+
+        pbar.close()
+        return avg_val_loss
+ 
+        
+    def test_step(self, dataloader):
+        self.model.eval()
+        # Initialize aggregated metrics
+        aggregated_metrics = {
+            'total_accuracy': 0.0,
+            'total_precision': 0.0,
+            'total_recall': 0.0,
+            'total_f1': 0.0,
+            'total_mcc': 0.0,
+            'total_top_2_acc': 0.0
+        }
+
+        with torch.no_grad():
+            pbar = tqdm(dataloader, desc="Testing")
+            for batch in pbar:
+                input_ids, attention_masks, labels = [x.to(self.device) for x in batch]
+                outputs = self.model(input_ids, attention_masks=attention_masks)
+
+                # Update and accumulate metrics
+                aggregated_metrics['total_accuracy'] += self.accuracy(outputs.logits.argmax(dim=1), labels).item()
+                aggregated_metrics['total_precision'] += self.precision(outputs.logits.argmax(dim=1), labels).item()
+                aggregated_metrics['total_recall'] += self.recall(outputs.logits.argmax(dim=1), labels).item()
+                aggregated_metrics['total_f1'] += self.f1(outputs.logits, labels).item()
+                aggregated_metrics['total_mcc'] += self.mcc(outputs.logits.argmax(dim=1), labels).item()
+                aggregated_metrics['total_top_2_acc'] += self.top2_acc(outputs.logits, labels).item()
+
+                # Update tqdm description with current metrics
                 pbar.set_postfix({
-                "Accuracy": accuracy,
-                'top_2_acc': top_2_acc
-            })
+                    'Accuracy': aggregated_metrics['total_accuracy'] / (pbar.n + 1),
+                    'MCC': aggregated_metrics['total_mcc'] / (pbar.n + 1)
+                })
 
-                # Update aggregated metrics
-                aggregated_metrics['accuracy'] += accuracy
-              
-                aggregated_metrics['precision'] += precision
-               
-                aggregated_metrics['recall'] += recall
-              
-                aggregated_metrics['f1'] += f1
-                
-                aggregated_metrics['mcc'] += mcc
-                
-                aggregated_metrics['top_2_acc'] += top_2_acc
-
-                pbar.update(1)
-
-            pbar.close()
+        # Calculate average metrics
         num_batches = len(dataloader)
         for key in aggregated_metrics:
             aggregated_metrics[key] /= num_batches
 
         return aggregated_metrics
+
 
     
 def main(mode = "full"):
